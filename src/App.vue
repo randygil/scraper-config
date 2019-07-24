161d77f23aa01e64ed9ac4bfb97cf8fa926fc171
Randy Gil <randygil@unerg.edu.ve>
2019-07-24 08:50:02 -0400

    build vuejs

--- src/App.vue
+++ src/App.vue
@@ -18,27 +18,31 @@
 
           <b-card-text>
             <b-row>
-              <b-form-group label="Status:">
-                <b-form-radio-group
-                  id="btn-radios-1"
-                  v-model="config.status"
-                  :options="[{text: 'Activo', value: 'Activo'}, {text: 'Inactivo', value: 'Inactivo'}]"
-                  buttons
-                   button-variant="outline-primary"
-                ></b-form-radio-group>
-              </b-form-group>
+              <b-col sm="12">
+                <b-form-group label="Status:">
+                  <b-form-radio-group
+                    id="btn-radios-1"
+                    v-model="config.status"
+                    :options="[{text: 'Activo', value: 'Activo'}, {text: 'Inactivo', value: 'Inactivo'}]"
+                    buttons
+                    button-variant="outline-primary"
+                  ></b-form-radio-group>
+                </b-form-group>
+              </b-col>
             </b-row>
 
             <b-row>
-              <b-form-group label="Intervalo:">
-                <b-form-input
-                  v-model="config.interval"
-                  type="number"
-                   placeholder="Intervalo en segundos"
-                > 
+              <b-col sm="12">
+                <b-form-group label="Intervalo:">
+                  <b-form-input
+                    v-model="config.interval"
+                    type="number"
+                    placeholder="Intervalo en segundos"
+                  > 
 
-                </b-form-input>
-              </b-form-group>
+                  </b-form-input>
+                </b-form-group>
+              </b-col>
             </b-row>
 
             <b-row>
@@ -54,9 +58,24 @@
                 class="d-flex justify-content-between align-items-center"
                 v-for="(item,index) in config.zones" v-bind:key="index">
                  
-                    {{ item.name }} ({{item.zipcode.split('\n').length}} zipcodes)  
-                  <div>
 
+                    <div>
+                      <b-form-group>
+                        
+                        <b-form-checkbox
+                        v-model="item.status"
+                        value="true"
+                        
+                      >
+                        {{ item.name }} ({{item.zipcode.split('\n').length}} zipcodes) 
+                       </b-form-checkbox> 
+
+                        </b-form-group> 
+                    </div> 
+                  <div>
+                    
+
+                     
                     <b-button @click="showEdit(item)">Editar</b-button>
                     <b-button @click="deleteItem(item)" variant="danger">Eliminar</b-button>
 
@@ -69,7 +88,7 @@
 
               <b-row class="mt-3">
                  <b-col sm="3">
-                    <label for="appliance" v-if="config.appliance">Appliance ({{config.appliance.split('\n').length}}):</label>
+                    <label for="appliance" >Appliance ({{config.appliance ? config.appliance.split('\n').length : ''}}):</label>
                  </b-col> 
                  <b-col sm="9">
                     <b-form-textarea
@@ -84,7 +103,7 @@
 
               <b-row class="mt-3">
                  <b-col sm="3">
-                    <label for="appliance" v-if="config.paymentMethod">Métodos de pago ({{config.paymentMethod.split('\n').length}}):</label>
+                    <label for="appliance" v-if="config.paymentMethod">Métodos de pago ({{config.paymentMethod ? config.paymentMethod.split('\n').length : ''}} ):</label>
                  </b-col> 
                  <b-col sm="9">
                     <b-form-textarea
@@ -105,11 +124,25 @@
         :title="editedIndex == -1 ? 'Nueva zona' : 'Editar zona'"
         ref="edit"
         id="edit"
-        @ok="saveEdit"
+       
         >
 
         <template v-if="editedItem">
           
+
+          <b-row class="mb-3">
+            <b-col sm="12">
+              <b-form-group label="Status:">
+                <b-form-radio-group
+                  id="btn-radios-1"
+                  v-model="editedItem.status"
+                  :options="[{text: 'Activo', value: true}, {text: 'Inactivo', value: ' '}]"
+                  buttons
+                   button-variant="outline-primary"
+                ></b-form-radio-group>
+              </b-form-group>
+            </b-col>
+          </b-row>
           <b-row class="mb-3">
             <b-col sm="3">
                 <label for="zipcode">Nombre:</label>
@@ -134,7 +167,7 @@
             </b-row>
             <b-row class="mt-3">
               <b-col sm="3">
-                <label for="zipcode">Zipcodes:</label>
+                <label for="zipcode">Fechas de activacion:</label>
               </b-col>
               <b-col sm="9">
                 <v-date-picker
@@ -142,7 +175,7 @@
                   v-model='editedItem.dates'>
                     <b-button
                       >
-                      Fechas de activacion
+                      Almanaque
                     </b-button>
                     
                 </v-date-picker>
@@ -171,11 +204,22 @@
             <b-button
               variant="primary"
               size="sm"
-              class="float-right"
+              :class="editedIndex != -1 ? 'float-right' : ''"
               @click="$refs['edit'].hide()"
             >
               Cerrar
             </b-button>
+
+            <b-button
+              
+              size="sm"
+              class="ml-3 float-right"
+              @click="saveEdit(); $refs['edit'].hide()"
+              v-if="editedIndex == -1"
+            > 
+              Guardar
+            </b-button>
+            
           </div>
       </b-modal>
    
@@ -239,6 +283,8 @@ export default {
         })
         this.editedItem = Object.assign(zone, {}) 
 
+      }else {
+        this.editedItem = {}
       }
       // JSON.parse(JSON.stringify(zone || {}))
    
